284527996e5176f2bdf5f9748c65ea148cbf8588
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-07-08 11:58:23 +0530

    test(ship): add failing tests for Ship module

---
+++ src/Ships/ship.test.js
@@ -0,0 +1,25 @@
+import {
+  Ship,
+  Carrier,
+  Battleship,
+  Cruiser,
+  Submarine,
+  Destroyer,
+} from "../Ships/ship.js";
+
+const newShip = new Ship();
+newShip.hit();
+newShip.hit();
+
+test("should return current hit counts", () => {
+  expect(newShip.hitCount).toBe(2);
+  expect(newShip.hit()).toBe(3)
+});
+
+test("should return length of the itself", () => {
+  expect(new Carrier().length).toBe(5);
+  expect(new Battleship().length).toBe(4);
+  expect(new Cruiser().length).toBe(3);
+  expect(new Submarine().length).toBe(2);
+  expect(new Destroyer().length).toBe(1);
+});
