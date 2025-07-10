3a6b0740022781acb57fd5e8882c5cc3c67e054c
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-07-10 11:10:12 +0530

    test(Player): test Player class contain gameboard

---
+++ src/Player/player.test.js
@@ -0,0 +1,7 @@
+import { Player } from "../Player/player.js";
+
+import { GameBoard } from "../Gameboard/gameboard";
+
+test("players should contain its own gameboard", () => {
+  expect(new Player().board instanceof GameBoard).toBe(true);
+});
